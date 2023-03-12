4d0c446359a4cd381fcd3f11e06d5135d06fd4d5
HaraHeique <heikacademicos@hotmail.com>
2023-03-12 17:00:34 -0300

    feat: alteração do title da aba

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-tmdb/', // Tem que ser o nome do repo no github
+  base: '/', // Tem que ser o nome do repo no github
   plugins: [react()],
 })
